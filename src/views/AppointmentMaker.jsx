53ed6e93f1946f416016879b6cb97e5c1f73917e
Luis <a222212359@unison.mx>
2024-12-09 23:17:47 -0700

    Update AppointmentMaker.jsx

--- src/views/AppointmentMaker.jsx
+++ src/views/AppointmentMaker.jsx
@@ -13,15 +13,25 @@ const AppointmentMaker = () => {
   const [extraServicesArray, setExtraServicesArray] = useState([]);
 
   //estos son los arrays que contienen los servicios que el USUARIO quiere
-  const [userServices, setUserServices] = useState([]);
-  const [userExtraServices, setUserExtraServices] = useState([]);
+  const [userOrders, setUserOrders] = useState([]); //cada objeto service es una card
+  //const [userExtraServices, setUserExtraServices] = useState([]);
+
+  //variables
+  const [totalCost, setTotalCost] = useState(0);
 
   //useEffect
   useEffect(() => {
     const fetchServices = async () => {
       const services = await getServices();
       if (services) {
-        setServicesArray(services);
+        const arrayOfServices = Object.entries(services).map(
+          ([key, value]) => ({
+            name: key,
+            ...value,
+          })
+        );
+
+        setServicesArray(arrayOfServices);
         setServicesLoaded(true);
       }
     };
@@ -32,7 +42,14 @@ const AppointmentMaker = () => {
     const fetchExtraServices = async () => {
       const extraServices = await getExtraServices();
       if (extraServices) {
-        setExtraServicesArray(extraServices);
+        const arrayOfExtraServices = Object.entries(extraServices).map(
+          ([key, value]) => ({
+            name: key,
+            ...value,
+          })
+        );
+
+        setExtraServicesArray(arrayOfExtraServices);
         setExtraServicesLoaded(true);
       }
     };
@@ -51,7 +68,62 @@ const AppointmentMaker = () => {
     }
   }, [extraServicesLoaded]);
 
+  useEffect(() => {
+    console.log(userOrders);
+  }, [userOrders]);
+
   //funciones
+
+  const addService = () => {
+    const orderObject = {
+      serviceName: "", //nombre del servicio
+      restTime: 0, //tiempo de descanso
+      hairLength: "", //dependiendo de si es true o false cambian price y duration
+      price: 0,
+      priceShort: 0,
+      priceMedium: 0,
+      priceLong: 0,
+      duration: 0,
+      durationShort: 0,
+      durationMedium: 0,
+      durationLong: 0,
+    };
+
+    setUserOrders([...userOrders, orderObject]);
+  };
+
+  const updateUserOrdersArray = ({ serviceName, orderIndex }) => {
+    const updatedUserOrders = [...userOrders];
+    const orderObject = userOrders.find((order, id) => id === orderIndex);
+    const serviceObject = servicesArray.find(
+      (service) => service.name === serviceName
+    );
+    orderObject.serviceName = serviceObject.name;
+    orderObject.restTime = serviceObject.restTime;
+    orderObject.hairLength = serviceObject.hairLength;
+    if (orderObject.hairLength) {
+      orderObject.priceShort = serviceObject.priceShort;
+      orderObject.priceMedium = serviceObject.priceMedium;
+      orderObject.priceLong = serviceObject.priceLong;
+      orderObject.durationShort = serviceObject.durationShort;
+      orderObject.durationMedium = serviceObject.durationMedium;
+      orderObject.durationLong = serviceObject.durationLong;
+    } else {
+      orderObject.price = serviceObject.price;
+      orderObject.duration = serviceObject.duration;
+    }
+
+    updatedUserOrders[orderIndex] = orderObject;
+    setUserOrders(updatedUserOrders);
+
+    // const orderObject = {
+    //   serviceName: "", //nombre del servicio
+    //   restTime: 0, //tiempo de descanso
+    //   hairLength: "", //dependiendo de si es true o false cambian price y duration
+    //   price: 0,
+    //   duration: 0,
+    // };
+  };
 
   return (
     <div className="w-full min-h-screen flex flex-col items-center">
@@ -60,49 +132,72 @@ const AppointmentMaker = () => {
       </h1>
 
       <div className="flex flex-col justify-center items-center w-[65%]">
-        <div className="relative w-[100%] border border-gray-900 mt-6 mb-2 flex flex-col p-5 rounded-md shadow-xl bg-gray-100">
-          <p>
-            Seleccione el servicio que desee (
-            <span className="text-red">*</span>)
-          </p>
-          <select
-            name="serviceName"
-            id=""
-            className="w-full border-2 border-black rounded-md text-center my-2 mb-6"
-          >
-            <option value="1">Corte de cabello</option>
-            <option value="2">Tinte de cabello</option>
-            <option value="3">Peinado</option>
-          </select>
-          <p>
-            Seleccione la longitud de su cabello (
-            <span className="text-red">*</span>)
-          </p>
-          <select
-            name="serviceName"
-            id=""
-            className="w-full  border-2 border-black rounded-md text-center my-2 mb-6"
-          >
-            <option value="1">Corto</option>
-            <option value="2">Mediano</option>
-            <option value="3">Largo</option>
-          </select>
-          <p>¿Algún servicio extra?</p>
-          <select
-            name="serviceName"
-            id=""
-            className="w-full  border-2 border-black rounded-md text-center my-2 mb-6"
-          >
-            <option value="1">Corte de cabello</option>
-            <option value="2">Tinte de cabello</option>
-            <option value="3">Peinado</option>
-          </select>
-        </div>
+        {userOrders.map((orderObject, orderIndex) => {
+          return (
+            <div
+              id={orderIndex}
+              className="relative w-[100%] border border-gray-900 mt-6 mb-2 flex flex-col p-5 rounded-md shadow-xl bg-gray-100"
+            >
+              <p>
+                Seleccione el servicio que desee (
+                <span className="text-red">*</span>)
+              </p>
+              <select
+                onChange={(e) => updateUserOrdersArray(e.value, orderIndex)}
+                name="serviceName"
+                id=""
+                className="w-full border-2 border-black rounded-md text-center my-2 mb-6"
+              >
+                {/* <option value="1">Corte de cabello</option> */}
+                {servicesArray.map((service, serviceIndex) => {
+                  return (
+                    <>
+                      <option id={serviceIndex} value={service.name}>
+                        {service.name}
+                      </option>
+                    </>
+                  );
+                })}
+              </select>
+              <p>
+                Seleccione la longitud de su cabello (
+                <span className="text-red">*</span>)
+              </p>
+              <select
+                name="serviceName"
+                id=""
+                className="w-full  border-2 border-black rounded-md text-center my-2 mb-6"
+              >
+                <option value="short">Corto</option>
+                <option value="medium">Mediano</option>
+                <option value="long">Largo</option>
+              </select>
+              <p>¿Algún servicio extra?</p>
+              <select
+                name="serviceName"
+                id=""
+                className="w-full  border-2 border-black rounded-md text-center my-2 mb-6"
+              >
+                {extraServicesArray.map((extraService, index) => {
+                  return (
+                    <option id={index} value={extraService.name}>
+                      {extraService.name}
+                    </option>
+                  );
+                })}
+              </select>
+            </div>
+          );
+        })}
 
         <h1 className="text-xl mt-6 ">
-          Costo Total: <span className="text-green font-black">$340</span>
+          Costo Total:{" "}
+          <span className="text-green font-black">${totalCost}</span>
         </h1>
-        <button className="px-2 py-1 rounded-md my-5 bg-blue text-white w-[140px]">
+        <button
+          onClick={addService}
+          className="px-2 py-1 rounded-md my-5 bg-blue text-white w-[140px]"
+        >
           Añadir Servicio
         </button>
 
