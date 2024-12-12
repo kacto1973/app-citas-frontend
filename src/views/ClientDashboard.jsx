0794f77d5df9db9e0127d2596f7e3c3a0ae17202
Luis <a222212359@unison.mx>
2024-12-12 13:28:41 -0700

    sorted appointments

--- src/views/ClientDashboard.jsx
+++ src/views/ClientDashboard.jsx
@@ -113,63 +113,71 @@ const ClientDashboard = () => {
         )}
         <div className="flex flex-col justify-center items-center w-full">
           {appointmentsOfClientLoaded &&
-            appointmentsOfClient.map((appointment, index) => (
-              <>
-                <div className="relative w-[80%] border border-gray-900 mt-6 flex flex-col p-5 rounded-md shadow-xl bg-gray-100">
-                  <div className="flex flex-row mb-2">
-                    <p>
-                      Cita #{index + 1} - {formatDate(appointment.selectedDate)}{" "}
-                      a las {appointment.selectedTime} <br /> (duración de{" "}
-                      {formatDuration(appointment.totalDurationOfAppointment)})
-                    </p>
-                    {/* <span className="text-white bg-blue py-0.5 px-1 ml-2 rounded-lg">
+            [...appointmentsOfClient] // Copia inmutable del arreglo original
+              .sort((a, b) => {
+                const dateA = new Date(`${a.selectedDate}T${a.selectedTime}`);
+                const dateB = new Date(`${b.selectedDate}T${b.selectedTime}`);
+                return dateA - dateB; // Orden ascendente
+              })
+              .map((appointment, index) => (
+                <>
+                  <div className="relative w-[80%] border border-gray-900 mt-6 flex flex-col p-5 rounded-md shadow-xl bg-gray-100">
+                    <div className="flex flex-row mb-2">
+                      <p>
+                        Cita #{index + 1} -{" "}
+                        {formatDate(appointment.selectedDate)} a las{" "}
+                        {appointment.selectedTime} <br /> (duración de{" "}
+                        {formatDuration(appointment.totalDurationOfAppointment)}
+                        )
+                      </p>
+                      {/* <span className="text-white bg-blue py-0.5 px-1 ml-2 rounded-lg">
                 Transfer
               </span> */}
-                    <p className="ml-auto">
-                      <span className="text-green font-black">
-                        ${appointment.totalCost}
-                      </span>
-                    </p>
-                  </div>
-                  {appointment.servicesCart &&
-                    appointment.servicesCart.map((service, serviceIndex) => (
-                      <p key={serviceIndex} className="w-[62%]">
-                        • {service.name} (${service.price})
+                      <p className="ml-auto">
+                        <span className="text-green font-black">
+                          ${appointment.totalCost}
+                        </span>
                       </p>
-                    ))}
-                  {appointment.extraServicesCart &&
-                    appointment.extraServicesCart.map(
-                      (extraService, extraServiceIndex) => (
-                        <p key={extraServiceIndex} className="w-[62%]">
-                          • {extraService.name} (${extraService.price})
+                    </div>
+                    {appointment.servicesCart &&
+                      appointment.servicesCart.map((service, serviceIndex) => (
+                        <p key={serviceIndex} className="w-[62%]">
+                          • {service.name} (${service.price})
                         </p>
-                      )
-                    )}
-                  <button
-                    className="py-1 rounded-md my-5 text-xs bg-blue text-white w-[90px] absolute bottom-0 right-[13%]"
-                    onClick={() => {
-                      downPayment(appointment.id);
-                    }}
-                  >
-                    Dejar Anticipo
-                  </button>
-                  <button
-                    className=" py-1 rounded-md my-5 text-xs bg-red text-white w-[30px] absolute bottom-0 right-[3%]"
-                    onClick={() => {
-                      const userConfirm = confirm(
-                        //aqui depende de si tiene anticipo o no pues mandar distintos alerts
-                        "¿Seguro que desea eliminar esta cita? Una vez dejado el anticipo, no se puede recuperar"
-                      );
-                      if (userConfirm) {
-                        cancelAppointment(appointment.id);
-                      }
-                    }}
-                  >
-                    X
-                  </button>
-                </div>
-              </>
-            ))}
+                      ))}
+                    {appointment.extraServicesCart &&
+                      appointment.extraServicesCart.map(
+                        (extraService, extraServiceIndex) => (
+                          <p key={extraServiceIndex} className="w-[62%]">
+                            • {extraService.name} (${extraService.price})
+                          </p>
+                        )
+                      )}
+                    <button
+                      className="py-1 rounded-md my-5 text-xs bg-blue text-white w-[90px] absolute bottom-0 right-[13%]"
+                      onClick={() => {
+                        downPayment(appointment.id);
+                      }}
+                    >
+                      Dejar Anticipo
+                    </button>
+                    <button
+                      className=" py-1 rounded-md my-5 text-xs bg-red text-white w-[30px] absolute bottom-0 right-[3%]"
+                      onClick={() => {
+                        const userConfirm = confirm(
+                          //aqui depende de si tiene anticipo o no pues mandar distintos alerts
+                          "¿Seguro que desea eliminar esta cita? Una vez dejado el anticipo, no se puede recuperar"
+                        );
+                        if (userConfirm) {
+                          cancelAppointment(appointment.id);
+                        }
+                      }}
+                    >
+                      X
+                    </button>
+                  </div>
+                </>
+              ))}
 
           <button
             className="px-2 py-1 rounded-md my-5 mt-10 bg-blue text-white w-[120px]"
